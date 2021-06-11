e5071471fd1b9f822d9a0e1385a7da863546f6ea
sonwen <you@example.com>
2021-06-11 16:43:03 +0800

    upload

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+    ]);
 
 if (mix.inProduction()) {
     mix.version();
